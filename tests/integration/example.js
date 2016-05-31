f02e3dfea1ed184c38871e765a29250af9aad4ca
Jess Stuart & Maxine Ellah <pair+jess-of-the-woods.maxine-ellah@devbootcamp.com>
2016-06-01 11:59:47 +1200

    second nightwatch test passing

--- tests/integration/example.js
+++ tests/integration/example.js
@@ -1,10 +1,19 @@
 module.exports = {
-  'Welcome page' : function (browser) {
-    browser
-      .url('http://localhost:3000/')
+  // 'Welcome page' : function (browser) {
+  //   browser
+  //     .url('http://localhost:3000/')
+  //     .waitForElementVisible('body', 1000)
+  //     .pause(1000)
+  //     .assert.containsText('h1', 'Welcome to Tandem')
+  //     .end();
+  // },
+
+  'Front page search' : function(browser) {
+    browser.url('http://localhost:3000/')
       .waitForElementVisible('body', 1000)
-      .pause(1000)
-      .assert.containsText('h1', 'Welcome to Tandem')
+      .setValue('#origin', 'Kaeo')
+      .pause(5000)
       .end();
+
   }
 };
