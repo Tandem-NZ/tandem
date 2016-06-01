4174131339ac916a6abf2944557bc358687030ab
Jess Stuart & Maxine Ellah <pair+jess-of-the-woods.maxine-ellah@devbootcamp.com>
2016-06-01 20:38:02 +1200

    ran integration tests

--- tests/integration/example.js
+++ tests/integration/example.js
@@ -1,56 +1,56 @@
 module.exports = {
-  // 'Welcome page' : function (browser) {
-  //   browser
-  //     .url('http://localhost:3000/')
-  //     .waitForElementVisible('body', 1000)
-  //     .pause(1000)
-  //     .assert.containsText('h1', 'Welcome to Tandem')
-  //     .end();
-  // },
-  //
-  // 'Front page search' : function(browser) {
-  //   browser.url('http://localhost:3000/')
-  //     .waitForElementVisible('body', 1000)
-  //     .setValue('#origin', 'Kaeo')
-  //     .pause(5000)
-  //     .click('#findButton')
-  //     .pause(1000)
-  //     .assert.urlEquals('http://localhost:3000/currentListings?origin=Kaeo&destination=')
-  //     .end();
-  // },
-  //
-  // 'Search page, only destination searched' : function(browser) {
-  //   browser.url('http://localhost:3000/')
-  //     .waitForElementVisible('body', 1000)
-  //     .setValue('#destination', 'Hamilton')
-  //     .pause(1000)
-  //     .click('#findButton')
-  //     .pause(1000)
-  //     .assert.containsText('p', 'Ooops...please enter a start point')
-  // },
-  //
-  // 'No search query error' : function(browser) {
-  //   browser.url('http://localhost:3000/')
-  //     .waitForElementVisible('body', 1000)
-  //     .setValue('#origin', '')
-  //     .pause(1000)
-  //     .click('#findButton')
-  //     .pause(1000)
-  //     .assert.containsText('p', 'Ooops...please enter a start point')
-  //     .end();
-  // },
-  //
-  // 'Origin and destination search > to singleListing page' : function(browser) {
-  //   browser.url('http://localhost:3000/')
-  //   .waitForElementVisible('body', 1000)
-  //   .setValue('#origin', 'Cromwell')
-  //   .setValue('#destination', 'Nightcaps')
-  //   .pause(1000)
-  //   .click('#findButton')
-  //   .pause(1000)
-  //   .assert.urlEquals('http://localhost:3000/currentListings?origin=Cromwell&destination=Nightcaps')
-  //   .end();
-  // },
+  'Welcome page' : function (browser) {
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .pause(1000)
+      .assert.containsText('h1', 'Welcome to Tandem')
+      .end();
+  },
+
+  'Front page search' : function(browser) {
+    browser.url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .setValue('#origin', 'Kaeo')
+      .pause(5000)
+      .click('#findButton')
+      .pause(1000)
+      .assert.urlEquals('http://localhost:3000/currentListings?origin=Kaeo&destination=')
+      .end();
+  },
+
+  'Search page, only destination searched' : function(browser) {
+    browser.url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .setValue('#destination', 'Hamilton')
+      .pause(1000)
+      .click('#findButton')
+      .pause(1000)
+      .assert.containsText('p', 'Ooops...please enter a start point')
+  },
+
+  'No search query error' : function(browser) {
+    browser.url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .setValue('#origin', '')
+      .pause(1000)
+      .click('#findButton')
+      .pause(1000)
+      .assert.containsText('p', 'Ooops...please enter a start point')
+      .end();
+  },
+
+  'Origin and destination search > to singleListing page' : function(browser) {
+    browser.url('http://localhost:3000/')
+    .waitForElementVisible('body', 1000)
+    .setValue('#origin', 'Cromwell')
+    .setValue('#destination', 'Nightcaps')
+    .pause(1000)
+    .click('#findButton')
+    .pause(1000)
+    .assert.urlEquals('http://localhost:3000/currentListings?origin=Cromwell&destination=Nightcaps')
+    .end();
+  },
 
   'Origin and destination search > to singleListing page with correct results' : function(browser) {
     browser.url('http://localhost:3000/')
