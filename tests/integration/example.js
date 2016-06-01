0f33950f51e99baf8a3fdaa5017da00802d1565e
Jess Stuart & Maxine Ellah <pair+jess-of-the-woods.maxine-ellah@devbootcamp.com>
2016-06-01 15:05:51 +1200

    No search query error test added and is passing

--- tests/integration/example.js
+++ tests/integration/example.js
@@ -1,19 +1,37 @@
 module.exports = {
-  // 'Welcome page' : function (browser) {
-  //   browser
-  //     .url('http://localhost:3000/')
-  //     .waitForElementVisible('body', 1000)
-  //     .pause(1000)
-  //     .assert.containsText('h1', 'Welcome to Tandem')
-  //     .end();
-  // },
+  'Welcome page' : function (browser) {
+    browser
+      .url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .pause(1000)
+      .assert.containsText('h1', 'Welcome to Tandem')
+      .end();
+  },
 
   'Front page search' : function(browser) {
     browser.url('http://localhost:3000/')
       .waitForElementVisible('body', 1000)
       .setValue('#origin', 'Kaeo')
       .pause(5000)
+      .click('#findButton')
+      .pause(1000)
+      .assert.urlEquals('http://localhost:3000/currentListings?origin=Kaeo&destination=')
       .end();
 
+  },
+
+  'No search query error' : function(browser) {
+    browser.url('http://localhost:3000/')
+      .waitForElementVisible('body', 1000)
+      .setValue('#origin', '')
+      .pause(1000)
+      .click('#findButton')
+      .pause(1000)
+      .assert.containsText('p', 'Ooops...please enter a start point')
+      .end();
   }
+
+
+
+
 };
