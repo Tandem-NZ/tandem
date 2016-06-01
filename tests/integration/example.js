f280f08a3d2bf272cd7ab8d39957aa73b146e692
Jess Stuart & Maxine Ellah <pair+jess-of-the-woods.maxine-ellah@devbootcamp.com>
2016-06-01 20:55:33 +1200

    ran integration tests

--- tests/integration/example.js
+++ tests/integration/example.js
@@ -1,69 +1,69 @@
 module.exports = {
-  'Welcome page' : function (browser) {
-    browser
-      .url('http://localhost:3000/')
-      .waitForElementVisible('body', 1000)
-      .pause(1000)
-      .assert.containsText('h1', 'Welcome to Tandem')
-      .end();
-  },
-
-  'Front page search' : function(browser) {
-    browser.url('http://localhost:3000/')
-      .waitForElementVisible('body', 1000)
-      .setValue('#origin', 'Kaeo')
-      .pause(5000)
-      .click('#findButton')
-      .pause(1000)
-      .assert.urlEquals('http://localhost:3000/currentListings?origin=Kaeo&destination=')
-      .end();
-  },
-
-  'Search page, only destination searched' : function(browser) {
-    browser.url('http://localhost:3000/')
-      .waitForElementVisible('body', 1000)
-      .setValue('#destination', 'Hamilton')
-      .pause(1000)
-      .click('#findButton')
-      .pause(1000)
-      .assert.containsText('p', 'Ooops...please enter a start point')
-  },
-
-  'No search query error' : function(browser) {
-    browser.url('http://localhost:3000/')
-      .waitForElementVisible('body', 1000)
-      .setValue('#origin', '')
-      .pause(1000)
-      .click('#findButton')
-      .pause(1000)
-      .assert.containsText('p', 'Ooops...please enter a start point')
-      .end();
-  },
-
-  'Origin and destination search > to singleListing page' : function(browser) {
-    browser.url('http://localhost:3000/')
-    .waitForElementVisible('body', 1000)
-    .setValue('#origin', 'Cromwell')
-    .setValue('#destination', 'Nightcaps')
-    .pause(1000)
-    .click('#findButton')
-    .pause(1000)
-    .assert.urlEquals('http://localhost:3000/currentListings?origin=Cromwell&destination=Nightcaps')
-    .end();
-  },
-
-  'Origin and destination search > to singleListing page with correct results' : function(browser) {
-    browser.url('http://localhost:3000/')
-    .waitForElementVisible('body', 1000)
-    .setValue('#origin', 'Taumarunui')
-    .setValue('#destination', 'Te Kuiti')
-    .pause(1000)
-    .click('#findButton')
-    .pause(1000)
-    .assert.containsText('h3', 'Taumarunui')
-    .assert.containsText('h3', ' Te Kuiti')
-    .end();
-  },
+  // 'Welcome page' : function (browser) {
+  //   browser
+  //     .url('http://localhost:3000/')
+  //     .waitForElementVisible('body', 1000)
+  //     .pause(1000)
+  //     .assert.containsText('h1', 'Welcome to Tandem')
+  //     .end();
+  // },
+  //
+  // 'Front page search' : function(browser) {
+  //   browser.url('http://localhost:3000/')
+  //     .waitForElementVisible('body', 1000)
+  //     .setValue('#origin', 'Kaeo')
+  //     .pause(5000)
+  //     .click('#findButton')
+  //     .pause(1000)
+  //     .assert.urlEquals('http://localhost:3000/currentListings?origin=Kaeo&destination=')
+  //     .end();
+  // },
+  //
+  // 'Search page, only destination searched' : function(browser) {
+  //   browser.url('http://localhost:3000/')
+  //     .waitForElementVisible('body', 1000)
+  //     .setValue('#destination', 'Hamilton')
+  //     .pause(1000)
+  //     .click('#findButton')
+  //     .pause(1000)
+  //     .assert.containsText('p', 'Ooops...please enter a start point')
+  // },
+  //
+  // 'No search query error' : function(browser) {
+  //   browser.url('http://localhost:3000/')
+  //     .waitForElementVisible('body', 1000)
+  //     .setValue('#origin', '')
+  //     .pause(1000)
+  //     .click('#findButton')
+  //     .pause(1000)
+  //     .assert.containsText('p', 'Ooops...please enter a start point')
+  //     .end();
+  // },
+  //
+  // 'Origin and destination search > to singleListing page' : function(browser) {
+  //   browser.url('http://localhost:3000/')
+  //   .waitForElementVisible('body', 1000)
+  //   .setValue('#origin', 'Cromwell')
+  //   .setValue('#destination', 'Nightcaps')
+  //   .pause(1000)
+  //   .click('#findButton')
+  //   .pause(1000)
+  //   .assert.urlEquals('http://localhost:3000/currentListings?origin=Cromwell&destination=Nightcaps')
+  //   .end();
+  // },
+  //
+  // 'Origin and destination search > to singleListing page with correct results' : function(browser) {
+  //   browser.url('http://localhost:3000/')
+  //   .waitForElementVisible('body', 1000)
+  //   .setValue('#origin', 'Taumarunui')
+  //   .setValue('#destination', 'Te Kuiti')
+  //   .pause(1000)
+  //   .click('#findButton')
+  //   .pause(1000)
+  //   .assert.containsText('h3', 'Taumarunui')
+  //   .assert.containsText('h3', ' Te Kuiti')
+  //   .end();
+  // },
 
   'Search bar on currentListings' : function(browser) {
     browser.url('http://localhost:3000/currentListings?origin=Taumarunui&destination=Whanganui')
@@ -72,7 +72,8 @@ module.exports = {
     .setValue('#destination', 'Hamilton')
     .click('#searchButton')
     .pause(1000)
-    .assert.containsText('h4', 'Kaeo - Hamilton')
+    .assert.containsText('h3', 'Kaeo ')
+    .assert.containsText('h3', ' Hamilton')
     .end();
   },
 
